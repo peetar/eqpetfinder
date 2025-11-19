c79667398701e45d2d34059524a823962e0ae713
Peter Trice <peter.trice@gmail.com>
2025-11-19 13:45:31 -0800

    Fix Vercel deployment - Add static file handling for CSS/JS - Update static path to absolute - Configure proper routing for assets

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Load NPC data from JSON file
 console.log('Loading NPC data...');
